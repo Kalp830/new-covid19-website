0b32a8db03de1aeaa221d766cbd52aee6242d3f1
Kalpesh Talekar <167209197+Kalp830@users.noreply.github.com>
2025-04-07 18:16:52 +0530

    Update vite.config.js
    slash change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/new-covid19-website",
+  base: "/new-covid19-website/",
 });
